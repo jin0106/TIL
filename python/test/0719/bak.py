8e9933f762754b2d511e45b24949f3af84515662
Dongjin Woo <53500163+jin0106@users.noreply.github.com>
2021-08-04 10:56:40 +0900

    Delete python/test directory

--- python/test/0719/bak.py
+++
@@ -1,9 +0,0 @@
-
-# a = int(input())
-# b = str(input())
-
-# print(a * int(b[2]))
-# # print(a * int(b[1]))
-# # print(a * int(b[0]))
-# print(a * int(b))
-print('apple'[0] in 'ssafy' and -3**3*0 > 4 & 2)
--- python/test/0723/1859. 백만 장자 프로젝트.py
+++
@@ -1,4 +0,0 @@
-T = int(input())
-
-for i in range(1, T+1):
-    N = map(int, input().split())
