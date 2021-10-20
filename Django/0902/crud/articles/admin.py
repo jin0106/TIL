6521e7e17d9765c672729ebf82e43c8a18b7acda
우동진 <wjinh93@gmail.com>
2021-10-21 01:29:37 +0900

    django classified

--- Django/0902/crud/articles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article
-# Register your models here.
-
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title',)
-admin.site.register(Article, ArticleAdmin)
